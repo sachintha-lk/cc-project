d94dd616aec27e7568b6cd984e23eb7815268aa9
Thenuri <cb010529@students.apiit.lk>
2023-07-24 09:33:39 +0530

    newdash

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -282,6 +282,7 @@
                  </ul>
               </div>
            </div>
+           
            {{-- <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
               <h3 class="text-xl leading-none font-bold text-gray-900 mb-10">Acquisition Overview</h3>
               <div class="block w-full overflow-x-auto">
--- resources/views/dashboard/teacher.blade.php
+++ resources/views/dashboard/teacher.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -12,4 +12,7 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-app-layout> --}}
+
+<x-sidebar>
+</x-sidebar>
