7184b4fd316e77f530f6c24716ace1f68c552781
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2023-08-02 22:17:11 +0530

    seeder and added debugbar dev dependancy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,181 +21,180 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        // $userroles = [
-        //     [
-        //         'id' => 1,
-        //         'name' => 'Admin',
-        //         'status' => true,
-        //     ],
-        //     [
-        //         'id' => 2,
-        //         'name' => 'Teacher',
-        //         'status' => true,
-        //     ],
-        //     [
-        //         'id' => 3,
-        //         'name' => 'Student',
-        //         'status' => true,
-        //     ]
+        $userroles = [
+            [
+                'id' => 1,
+                'name' => 'Admin',
+                'status' => true,
+            ],
+            [
+                'id' => 2,
+                'name' => 'Teacher',
+                'status' => true,
+            ],
+            [
+                'id' => 3,
+                'name' => 'Student',
+                'status' => true,
+            ]
 
-        // ];
+        ];
 
-        // foreach ($userroles as $role) {
-        //     \App\Models\Role::create($role);
-        // }
+        foreach ($userroles as $role) {
+            \App\Models\Role::create($role);
+        }
 
-        // // Create admin user
-        // \App\Models\User::create([
-        //     'name' => 'Admin',
-        //     'email' => 'admin@educatelanka.lk',
-        //     'password' => Hash::make('adminpassword'),
-        //     'role_id' => 1,
-        //     'status' => true,
-        // ]);
+        // Create admin user
+        \App\Models\User::create([
+            'name' => 'Admin',
+            'email' => 'admin@educatelanka.lk',
+            'password' => Hash::make('adminpassword'),
+            'role_id' => 1,
+            'status' => true,
+        ]);
 
-        // // Create teacher user
-        // \App\Models\User::create([
-        //     'name' => 'Teacher1',
-        //     'email' => 'teacher1@school1.com',
-        //     'password' => Hash::make('teacherpassword'),
-        //     'role_id' => 2,
-        //     'status' => true,
-        // ]);
+        // Create teacher user
+        \App\Models\User::create([
+            'name' => 'Teacher1',
+            'email' => 'teacher1@school1.com',
+            'password' => Hash::make('teacherpassword'),
+            'role_id' => 2,
+            'status' => true,
+        ]);
 
-        // // Create student user
-        // \App\Models\User::create([
-        //     'name' => 'Student1',
-        //     'email' => 'student1@school1.com',
-        //     'password' => Hash::make('studentpassword'),
-        //     'role_id' => 3,
-        //     'status' => true,
-        // ]);
+        // Create student user
+        \App\Models\User::create([
+            'name' => 'Student1',
+            'email' => 'student1@school1.com',
+            'password' => Hash::make('studentpassword'),
+            'role_id' => 3,
+            'status' => true,
+        ]);
 
-        // // Create student user
-        // \App\Models\User::create([
-        //     'name' => 'Student2',
-        //     'email' => 'student2@school1.com',
-        //     'password' => Hash::make('studentpassword'),
-        //     'role_id' => 3,
-        //     'status' => true,
-        // ]);
+        // Create student user
+        \App\Models\User::create([
+            'name' => 'Student2',
+            'email' => 'student2@school1.com',
+            'password' => Hash::make('studentpassword'),
+            'role_id' => 3,
+            'status' => true,
+        ]);
 
-        // // Create student user
-        // \App\Models\User::create([
-        //     'name' => 'Student3',
-        //     'email' => 'student3@school1.com',
-        //     'password' => Hash::make('studentpassword'),
-        //     'role_id' => 3,
-        //     'status' => true,
-        // ]);
+        // Create student user
+        \App\Models\User::create([
+            'name' => 'Student3',
+            'email' => 'student3@school1.com',
+            'password' => Hash::make('studentpassword'),
+            'role_id' => 3,
+            'status' => true,
+        ]);
 
 
-        // $grades = [
-        //     [
-        //     'id' => 1,
-        //     'name' => 'Grade 1',
-        //     ],
-        //     [
-        //     'id' => 2,
-        //     'name' => 'Grade 2',
-        //     ],
-        //     [
-        //     'id' => 3,
-        //     'name' => 'Grade 3',
-        //     ],
-        //     [
-        //     'id' => 4,
-        //     'name' => 'Grade 4',
-        //     ],
-        //     [
-        //     'id' => 5,
-        //     'name' => 'Grade 5',
-        //     ],
-        //     [
-        //     'id' => 6,
-        //     'name' => 'Grade 6',
-        //     ],
-        //     [
-        //     'id' => 7,
-        //     'name' => 'Grade 7',
-        //     ],
-        //     [
-        //     'id' => 8,
-        //     'name' => 'Grade 8',
-        //     ],
-        //     [
-        //     'id' => 9,
-        //     'name' => 'Grade 9',
-        //     ],
-        //     [
-        //     'id' => 10,
-        //     'name' => 'Grade 10',
-        //     ],
-        //     [
-        //     'id' => 11,
-        //     'name' => 'Grade 11',
-        //     ],
-        //     [
-        //     'id' => 12,
-        //     'name' => 'Grade 12',
-        //     ],
-        //     [
-        //     'id' => 13,
-        //     'name' => 'Grade 13',
-        //     ],
-        // ];
+        $grades = [
+            [
+                'id' => 1,
+                'name' => 'Grade 1',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Grade 2',
+            ],
+            [
+                'id' => 3,
+                'name' => 'Grade 3',
+            ],
+            [
+                'id' => 4,
+                'name' => 'Grade 4',
+            ],
+            [
+                'id' => 5,
+                'name' => 'Grade 5',
+            ],
+            [
+                'id' => 6,
+                'name' => 'Grade 6',
+            ],
+            [
+                'id' => 7,
+                'name' => 'Grade 7',
+            ],
+            [
+                'id' => 8,
+                'name' => 'Grade 8',
+            ],
+            [
+                'id' => 9,
+                'name' => 'Grade 9',
+            ],
+            [
+                'id' => 10,
+                'name' => 'Grade 10',
+            ],
+            [
+                'id' => 11,
+                'name' => 'Grade 11',
+            ],
+            [
+                'id' => 12,
+                'name' => 'Grade 12',
+            ],
+            [
+                'id' => 13,
+                'name' => 'Grade 13',
+            ],
+        ];
 
-        // foreach ($grades as $grade) {
-        //     \App\Models\Grade::create($grade);
-        // }
+        foreach ($grades as $grade) {
+            \App\Models\Grade::create($grade);
+        }
 
 
 
         $class = [
-            // [
-            //    'id' => 1,
-            //    'class_name' => 'Class A',
-            //    'grade_id' => 1,
+            [
+                'id' => 1,
+                'class_name' => 'Class A',
+                'grade_id' => 1,
 
-            // ],
-            // [
-            //    'id' => 2,
-            //    'class_name' => 'Grade B',
-            //    'grade_id' => 1,
+            ],
+            [
+                'id' => 2,
+                'class_name' => 'Grade B',
+                'grade_id' => 1,
 
-            // ],
-            
-           
+            ],
+
+
         ];
 
         foreach ($class as $class) {
-             \App\Models\GradeClasses::create($class);
+            \App\Models\GradeClasses::create($class);
         }
 
-        $class=[
+        $class = [
 
             [
-            'id' => 007,
-            'class_name' => 'Class C',
-            'grade_id' => 1,
-        
+                'id' => 007,
+                'class_name' => 'Class C',
+                'grade_id' => 1,
+
             ],
             [
-            'id' => 8,
-            'class_name' => 'Grade D',
-            'grade_id' => 1,
-        
+                'id' => 8,
+                'class_name' => 'Grade D',
+                'grade_id' => 1,
+
             ],
             [
                 'id' => 9,
                 'class_name' => 'Grade E',
                 'grade_id' => 1,
-            
+
             ]
         ];
         foreach ($class as $class) {
-                  \App\Models\GradeClasses::create($class);
-         }
-
+            \App\Models\GradeClasses::create($class);
+        }
     }
 }
