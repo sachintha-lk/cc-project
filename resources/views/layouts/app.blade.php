3085863bfb929d0cf8257d6ec93de73a4e3b8ea5
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2023-08-03 09:25:04 +0530

    removed alpine cdn link to fix the bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
         <!-- Styles -->
         @livewireStyles
     </head>
