4dedaa32bf753dbcb5f6aa0791deea6c29f0181c
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2023-09-24 16:13:01 +0530

    Fix bug module id

--- app/Policies/CategoryPolicy.php
+++ app/Policies/CategoryPolicy.php
@@ -2,10 +2,19 @@
 
 namespace App\Policies;
 
+use App\Models\Module;
 use TeamTeaTime\Forum\Models\Category;
 use TeamTeaTime\Forum\Policies\CategoryPolicy as Base;
+
 class CategoryPolicy extends Base
 {
+    protected $module;
+
+    public function __construct(Module $module)
+    {
+        $this->module = $module;
+    }
+
     public function deleteThreads($user, Category $category): bool
     {
         return true;
@@ -13,71 +22,27 @@ class CategoryPolicy extends Base
 
     public function enableThreads($user, Category $category): bool
     {
-        if (in_array($user->role->name, ['Admin'])) {
-            return true;
-        }
-
-        if ($category->is_for_module && $user->role->name == 'Teacher' && $user->teacher->modules->contains($category->module)) {
-            return true;
-        }
-
-        return false;
-
+        return $this->canPerformAction($user, $category);
     }
 
     public function moveThreadsFrom($user, Category $category): bool
     {
-        if (in_array($user->role->name, ['Admin'])) {
-            return true;
-        }
-
-        if ($category->is_for_module && $user->role->name == 'Teacher' && $user->teacher->modules->contains($category->module)) {
-            return true;
-        }
-
-        return false;
-
+        return $this->canPerformAction($user, $category);
     }
 
     public function moveThreadsTo($user, Category $category): bool
     {
-        if (in_array($user->role->name, ['Admin'])) {
-            return true;
-        }
-
-        if ($category->is_for_module && $user->role->name == 'Teacher' && $user->teacher->modules->contains($category->module)) {
-            return true;
-        }
-
-        return false;
+        return $this->canPerformAction($user, $category);
     }
 
     public function lockThreads($user, Category $category): bool
     {
-        if (in_array($user->role->name, ['Admin'])) {
-            return true;
-        }
-
-        if ($category->is_for_module && $user->role->name == 'Teacher' && $user->teacher->modules->contains($category->module)) {
-            return true;
-        }
-
-        return false;
-
+        return $this->canPerformAction($user, $category);
     }
 
     public function pinThreads($user, Category $category): bool
     {
-        if (in_array($user->role->name, ['Admin'])) {
-            return true;
-        }
-
-        if ($category->is_for_module && $user->role->name == 'Teacher' && $user->teacher->modules->contains($category->module)) {
-            return true;
-        }
-
-        return false;
-
+        return $this->canPerformAction($user, $category);
     }
 
     public function view($user, Category $category): bool
@@ -88,5 +53,20 @@ class CategoryPolicy extends Base
     public function delete($user, Category $category): bool
     {
         return $user->role->name == 'Admin';
+    }
+
+    protected function canPerformAction($user, Category $category): bool
+    {
+        if (in_array($user->role->name, ['Admin'])) {
+            return true;
+        }
+
+        $module = $this->module->where('id', $category->module_id)->first();
+
+        if ($category->is_for_module && $user->role->name == 'Teacher' && $user->id === $module->teacher_id) {
+            return true;
+        }
+
+        return false;
     }
 }
