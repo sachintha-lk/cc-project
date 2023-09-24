3dbcdc4322885ca1404218f192497b1f0aae8295
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2023-09-24 10:20:18 +0530

    fix roleid add migration

---
+++ database/migrations/2023_07_20_171401_add_role_id_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('role_id')->default(3)->constrained('roles');;
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $this->dropForeign('role_id');
+        });
+    }
+};
