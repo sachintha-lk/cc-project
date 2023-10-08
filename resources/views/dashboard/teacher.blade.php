a0202be6175066eb450cbc7d1d93e62297b492c2
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2023-10-08 16:41:15 +0530

    add link to assignment

--- resources/views/dashboard/teacher.blade.php
+++ resources/views/dashboard/teacher.blade.php
@@ -101,7 +101,13 @@
                             @foreach($latestSubmissions as $submission)
                             <tr class="text-gray-500">
                                 <th class="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">
+                                    <a href="{{route('show-submission', [
+                                    'module_id' => $submission->assignment->module->id,
+                                    'assignment_id' => $submission->assignment->id,
+
+                                     ] )}}">
                                     {{ $submission->assignment->assignment_name }}
+                                    </a>
                                 </th>
                                 <td class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">
                                     {{ $submission->student->student_id }}
@@ -114,16 +120,7 @@
                                 <td class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">
                                     {{ $submission->created_at->diffForHumans() }}
                                 </td>
-                                {{--                          <td class="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">--}}
-                                {{--                             <div class="flex items-center">--}}
 
-                                {{--                                <div class="relative w-full">--}}
-                                {{--                                   <div class="w-full bg-gray-200 rounded-sm h-2">--}}
-                                {{--                                      <div class="bg-cyan-600 h-2 rounded-sm" style="width: 30%"></div>--}}
-                                {{--                                   </div>--}}
-                                {{--                                </div>--}}
-                                {{--                             </div>--}}
-                                {{--                          </td>--}}
                             </tr>
                             @endforeach
                         @else
